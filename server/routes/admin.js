67650c07de96adb5b034e3493205239b914ab4da
Hala Qitouqa <h.qitouqa@gmail.com>
2024-02-04 19:22:03 +0300

    added first two routes

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const adminController = require("../controllers/admin");
 
+router.post("/add", adminController.addNewShopItem);
+router.put("/update/:id", adminController.updateShopItem);
 router.get('/', adminController.getData)
 router.delete('/remove/:id', adminController.removeOneItem);
 router.delete('/removeManyItems', adminController.removeManyItems);
