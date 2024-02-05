e87d7be535492244a1b42973db6e6644c4045057
Hassan AbuGareeb <Hassan.AbuGareeb@gmail.com>
2024-02-06 02:36:48 +0300

    minor changes

---
+++ server/routes/customer.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", customerController.getAllShopItems); //get all shop items
+router.get("/filter", customerController.filterShopItems); //filter shop items
+router.get("/search", customerController.serchShopItems); //search for a shop item
+router.post("/:id/cart", customerController.addItemToCart); //send item id and quantity in after the cart in the request params
+router.post("/:id/checkout", customerController.checkoutCustomer); //calcaulte the bill for the customer
+router.get("/:id", customerController.getShopItemInfo); //get information of the item
+
+module.exports = router;
